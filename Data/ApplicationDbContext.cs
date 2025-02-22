6b36386e3c9849e9602495d242a4996fe86f71fe
lmieczkowski <mieczkowski107@gmail.com>
2025-02-22 21:47:59 +0100

    Add DbContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReservationApp.Models;
 
 namespace ReservationApp.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Company> Companies { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
+        { 
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Tattoo" },
+                new Category { Id = 2, Name = "Barber" },
+                new Category { Id = 3, Name = "Physiotherapy" }
+            );
+           
         }
     }
 }
---
+++ Data/Migrations/20250220124449_AddAndSeedCategoryToDb.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ReservationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAndSeedCategoryToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Tattoo" },
+                    { 2, "Barber" },
+                    { 3, "Physiotherapy" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
