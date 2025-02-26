3459bcc35e4b2600406f8ea1a699ea2e4fb0764b
lmieczkowski <mieczkowski107@gmail.com>
2025-02-26 23:37:57 +0100

    Own User Model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber
+        { get
+            {
+                return base.PhoneNumber;
+            }
+
+            set
+            {
+                base.PhoneNumber = value;
+            }
+        }
+
+        [Display(Name = "Date of Birth")]
+        public DateOnly? DateOfBirth { get; set; }
+        
+    }
+}
