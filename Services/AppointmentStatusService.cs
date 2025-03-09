fc29a7f934afb3c97b0a863c003b147cbe2a0a03
lmieczkowski <mieczkowski107@gmail.com>
2025-03-09 15:54:27 +0100

    Add backgroundservice to change status

---
+++ Services/AppointmentStatusService.cs
@@ -0,0 +1,56 @@
+﻿using ReservationApp.Data;
+using ReservationApp.Data.Repository.IRepository;
+
+namespace ReservationApp.Services;
+
+public class AppointmentStatusService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    //private readonly IUnitOfWork _unitOfWork;
+    public AppointmentStatusService(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        var _unitOfWork = scope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+        #region Delete past appointments 
+        DateOnly today = DateOnly.FromDateTime(DateTime.UtcNow);
+        var pastAppointments = _unitOfWork.Appointments.GetAll(a => a.Date < today, tracked: false);
+        foreach (var appointment in pastAppointments)
+        {
+            // If the appointment is not confirmed or paid, it is considered to be cancelled
+            if (appointment.Status == Utility.AppointmentStatus.Pending)
+            {
+                appointment.Status = Utility.AppointmentStatus.Cancelled;
+            }
+            // By default, if the company does not mark as a no-show, it is considered to be done
+            else if (appointment.Status == Utility.AppointmentStatus.Confirmed)
+            {
+                appointment.Status = Utility.AppointmentStatus.Completed;
+            }
+            _unitOfWork.Appointments.Update(appointment);
+        }
+        _unitOfWork.Save();
+
+        #endregion
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            DateTime dateNow = DateTime.UtcNow;
+            foreach (var appointment in _unitOfWork.Appointments.GetAll(u => u.CreatedAt.Date == dateNow.Date))
+            {
+                if (appointment.Status == Utility.AppointmentStatus.Pending && ((dateNow - appointment.CreatedAt).TotalMinutes > TimeSpan.FromMinutes(15).TotalMinutes))
+                {
+                    appointment.Status = Utility.AppointmentStatus.Cancelled;
+                    _unitOfWork.Appointments.Update(appointment);
+                }
+            }
+            _unitOfWork.Save();
+            await Task.Delay((int)TimeSpan.FromMinutes(15).TotalMilliseconds, stoppingToken);
+        }
+    }
+}
+
