83f18f395c34f53cd77f5451240f71780041660d
lmieczkowski <mieczkowski107@gmail.com>
2025-03-09 23:24:25 +0100

    Check IsValid before checking availability of hour

--- Models/ViewModels/AppointmentVM.cs
+++ Models/ViewModels/AppointmentVM.cs
@@ -5,4 +5,26 @@ public class AppointmentVM
     public int ServiceId { get; set; }
     public DateOnly SelectedDate { get; set; }
     public TimeOnly SelectedTime { get; set; }
+
+    public bool IsValid ()
+    {
+        if(SelectedDate == null || SelectedTime == null)
+        {
+            return false;
+        }
+        if (SelectedDate < DateOnly.FromDateTime(DateTime.UtcNow.AddDays(2)) || SelectedDate > DateOnly.FromDateTime(DateTime.Now.AddDays(30)))
+        {
+            return false;
+        }
+        if(SelectedDate.DayOfWeek == DayOfWeek.Saturday || SelectedDate.DayOfWeek == DayOfWeek.Sunday)
+        {
+            return false;
+        }
+        if (SelectedTime < new TimeOnly(8, 0, 0) || SelectedTime > new TimeOnly(16, 0, 0))
+        {
+            return false;
+        }
+
+        return true;
+    }
 }
