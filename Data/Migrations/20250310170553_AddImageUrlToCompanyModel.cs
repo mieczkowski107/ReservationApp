a63b18de03ab7deb83e22e6deb9cb4b66b0053e8
lmieczkowski <mieczkowski107@gmail.com>
2025-03-10 18:50:34 +0100

    Add Image Url To Company Model

---
+++ Data/Migrations/20250310170553_AddImageUrlToCompanyModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToCompanyModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "\\images\\company\\Temporary.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\images\\compnay\\Temporary.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "\\images\\company\\Temporary.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Companies");
+        }
+    }
+}
