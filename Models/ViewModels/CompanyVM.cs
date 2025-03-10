f43294a1bfec7ca446ffb6744f917a02b1ee5713
lmieczkowski <mieczkowski107@gmail.com>
2025-03-10 18:52:22 +0100

    Refactor

--- Models/ViewModels/CompanyVM.cs
+++ Models/ViewModels/CompanyVM.cs
@@ -1,11 +1,13 @@
 ﻿
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics.CodeAnalysis;
 
-namespace ReservationApp.Models.ViewModels
+namespace ReservationApp.Models.ViewModels;
+
+public class CompanyVM
 {
-    public class CompanyVM
-    {
-        public Company? Company { get; set; }
-        public IEnumerable<SelectListItem> CategoryList { get; set; }
-    }
+    public Company? Company { get; set; }
+    [ValidateNever]
+    public IEnumerable<SelectListItem> CategoryList { get; set; }
 }
