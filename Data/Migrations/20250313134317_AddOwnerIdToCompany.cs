e8baef9ba7d4f24f1eb6e4214c7aa55c13afe855
lmieczkowski <mieczkowski107@gmail.com>
2025-03-15 17:40:42 +0100

    Migrations

---
+++ Data/Migrations/20250313134317_AddOwnerIdToCompany.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerIdToCompany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OwnerId",
+                table: "Companies",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+            column: "OwnerId",
+                value: "38cbf525-8cb0-4eef-a6af-b7067e8317e1");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OwnerId",
+                value: "38cbf525-8cb0-4eef-a6af-b7067e8317e1");
+
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "OwnerId",
+                value: "a7a42594-abc8-4eb2-ba0c-1f5fce0aad75");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Companies");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
