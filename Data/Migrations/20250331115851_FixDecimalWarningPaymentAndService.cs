93712a05709d3bb5f4fe9e80af5f007a4bae5bad
lmieczkowski <mieczkowski107@gmail.com>
2025-03-31 13:59:23 +0200

    Fix: decimal warning

---
+++ Data/Migrations/20250331115851_FixDecimalWarningPaymentAndService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDecimalWarningPaymentAndService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
