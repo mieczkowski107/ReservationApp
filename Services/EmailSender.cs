9295fa18661cb9b1fc157f79a5f95d94b166b422
lmieczkowski <mieczkowski107@gmail.com>
2025-04-01 15:31:34 +0200

    Hide SensitiveData

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -6,10 +6,17 @@ namespace ReservationApp.Services;
 
 public class EmailSender : IEmailSender
 {
+    private readonly IConfiguration _configuration;
+
+    public EmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var mail = "serverguard@zohomail.eu";
-        
+        var mail = _configuration.GetSection("Email")["Mail"];
+        var pw = _configuration.GetSection("Email")["Password"];
         var client = new SmtpClient("smtp.zoho.eu", 587)
         {
             Credentials = new NetworkCredential(mail, pw),
