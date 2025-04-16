7c093a9626aad4983b02b6285800e8bf38a67de3
lmieczkowski <mieczkowski107@gmail.com>
2025-04-16 23:08:05 +0200

    BindingProperty to catch date range

--- Areas/Admin/Controllers/ReportController.cs
+++ Areas/Admin/Controllers/ReportController.cs
@@ -3,14 +3,21 @@ using ReservationApp.Data.Repository.IRepository;
 
 namespace ReservationApp.Areas.Admin.Controllers;
 
+[Area("Admin")]
 public class ReportController(IUnitOfWork unitOfWork) : Controller
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
+    [BindProperty]
+    public DateOnly StartRangeDate { get; set; }
+    [BindProperty]
+    public DateOnly EndRangeDate { get; set; }
+
     public IActionResult Index()
     {
+        var x = StartRangeDate;
+        var y = EndRangeDate;
         return View();
     }
-
    
 }
