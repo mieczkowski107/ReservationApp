60b27a5c2b14e71d5680ca55da1a2977edba6039
lmieczkowski <mieczkowski107@gmail.com>
2025-04-16 23:09:56 +0200

    Migrations

---
+++ Data/Migrations/20250402121824_AddReportModelToDb.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReservationApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReportModelToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    StartRangeDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EndRangeDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Income = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Appointments = table.Column<int>(type: "int", nullable: true),
+                    DeletedAppointments = table.Column<int>(type: "int", nullable: true),
+                    NoShowAppointments = table.Column<int>(type: "int", nullable: true),
+                    TotalClients = table.Column<int>(type: "int", nullable: true),
+                    UniqueClients = table.Column<int>(type: "int", nullable: true),
+                    NewClients = table.Column<int>(type: "int", nullable: true),
+                    AvgRating = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Note = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Report_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Report_CompanyId",
+                table: "Report",
+                column: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
