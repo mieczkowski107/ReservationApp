e9e95fd729fcc2de1fe9d9ff7c433256912f66ad
lmieczkowski <mieczkowski107@gmail.com>
2025-04-23 23:10:13 +0200

    MockUp of returning View with raport

--- Areas/Admin/Controllers/ReportController.cs
+++ Areas/Admin/Controllers/ReportController.cs
@@ -1,23 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ReservationApp.Data.Repository.IRepository;
+using ReservationApp.Models;
+using ReservationApp.Services;
 
 namespace ReservationApp.Areas.Admin.Controllers;
 
 [Area("Admin")]
-public class ReportController(IUnitOfWork unitOfWork) : Controller
+public class ReportController(IUnitOfWork unitOfWork, IReportService reportService) : Controller
 {
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IReportService reportService = reportService;
 
     [BindProperty]
-    public DateOnly StartRangeDate { get; set; }
+    public DateOnly? StartRangeDate { get; set; }
     [BindProperty]
-    public DateOnly EndRangeDate { get; set; }
+    public DateOnly? EndRangeDate { get; set; }
 
     public IActionResult Index()
     {
-        var x = StartRangeDate;
-        var y = EndRangeDate;
-        return View();
+        if(!StartRangeDate.HasValue|| !EndRangeDate.HasValue)
+        {
+            var Raport = new Report();
+            return View(Raport);
+        }
+        else
+        {
+            var Raport = reportService.GetReport(1, StartRangeDate.Value, EndRangeDate.Value);
+            return View(Raport);
+        }
+        
     }
-   
+
 }
