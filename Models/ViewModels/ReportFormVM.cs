68c3c6a2519a576eafbda58a639aa2b4f3f56059
lmieczkowski <mieczkowski107@gmail.com>
2025-04-27 19:30:41 +0200

    View Models for Report ReportFormVM is for Form ReportVm is for mapping it to csv and to view

---
+++ Models/ViewModels/ReportFormVM.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReservationApp.Models.ViewModels;
+
+public class ReportFormVM
+{
+    [ValidateNever]
+    public IEnumerable<SelectListItem>? CompanyList { get; set; }
+    [Required]
+    public int CompanyId { get; set; }
+    [Required]
+    [DisplayName("Start Date")]
+    public DateOnly StartRangeDate { get; set; }
+    [Required]
+    [DisplayName("End Date")]
+    public DateOnly EndRangeDate { get; set; }
+
+}
---
+++ Models/ViewModels/ReportVm.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReservationApp.Models.ViewModels;
+
+public class ReportVm
+{
+    public ReportVm(Report report)
+    {
+        Id = report.Id;
+        CompanyName = report.Company?.Name;
+        CreatedAt = report.CreatedAt;
+        StartRangeDate = report.StartRangeDate;
+        EndRangeDate = report.EndRangeDate;
+        Income = report.Income;
+        Appointments = report.Appointments;
+        DeletedAppointments = report.DeletedAppointments;
+        NoShowAppointments = report.NoShowAppointments;
+        TotalClients = report.TotalClients;
+        UniqueClients = report.UniqueClients;
+        NewClients = report.NewClients;
+        AvgRating = report.AvgRating;
+    }
+    public int Id { get; set; }
+    [Required]
+    public string? CompanyName{ get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    [Required]
+    public DateOnly StartRangeDate { get; set; }
+    [Required]
+    public DateOnly EndRangeDate { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal? Income { get; set; }
+    public int? Appointments { get; set; }
+    public int? DeletedAppointments { get; set; }
+    public int? NoShowAppointments { get; set; }
+    public int? TotalClients { get; set; }
+    public int? UniqueClients { get; set; }
+    public int? NewClients { get; set; }
+
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal? AvgRating { get; set; }
+    public string? ReportUrl { get; set; }
+
+    
+}
